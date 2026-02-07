c6a9f9fa86dcacea7c809b8885ef4445cef3a76c
Jimmy Phan <phanjimmy02@gmail.com>
2026-02-07 17:26:27 -0500

    colums and rows for grid

--- tictactoe.py
+++ tictactoe.py
@@ -20,9 +20,19 @@ player = random.choice(players) #randomly select a player to start the game
 buttons = [[0, 0, 0], #buttons is a 3x3 list that will hold the button objects for the tic tac toe grid
            [0, 0, 0],
            [0, 0, 0]]
+
 Label = Label(text= player + " turn", font=('consolas', 40)) #Label is a label widget that will display the current player's turn
 Label.pack(side="top")
 
 reset_button = Button(text="Restart", font=('consolas', 20), command=new_game) #reset_button is a button widget that will call the new_game function when clicked to reset the game
 reset_button.pack(side="top") #pack the reset button at the top of the window
+
+Frame = Frame(window) #Frame is a frame widget that will hold the tic tac toe grid
+Frame.pack()
+
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(Frame, text="", font=('consolas', 40), width=5, height=2,
+                                      command=lambda row=row, column=column: next_turn(row, column)) #create a button for each cell in the tic tac toe grid and set its command to call the next_turn function with the row and column as arguments
+        buttons[row][column].grid(row=row, column=column) #place the button in the grid using the grid geometry manager
 window.mainloop()
